a3f5cd6db507ac9b76dbde7c736dd7d448cf0a5b
Zhang-Deheng <1367117226@qq.com>
2020-12-28 16:57:27 +0800

    111

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,22 +1,26 @@
 import React, { Component } from 'react'
+import Swiper from '../utils/Swiper.js'
 import { connect } from 'react-redux'
-
+import { Layout } from 'antd';
+const { Header, Footer, Content } = Layout;
 export class Home extends Component {
     render() {
         return (
-            <div>
-                首页
-            </div>
+                <Layout>
+                <Header>Header</Header>
+                <Layout>
+                    <Content><Swiper /></Content>
+                </Layout>
+                <Footer>Footer</Footer>
+                </Layout>
         )
     }
 }
 
 const mapStateToProps = (state) => ({
-    
 })
-
 const mapDispatchToProps = {
-    
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home)
